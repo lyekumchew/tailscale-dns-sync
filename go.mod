44c157943b0e15c8e29139e227af7189283a6775
lyekumchew <lyekumchew@gmail.com>
2023-04-04 17:26:57 +0800

    init

---
+++ go.mod
@@ -0,0 +1,40 @@
+module tailscale-dns-sync
+
+go 1.20
+
+require (
+	github.com/cloudflare/cloudflare-go v0.64.0
+	github.com/deckarep/golang-set/v2 v2.3.0
+	tailscale.com v1.38.3
+)
+
+require (
+	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/akutz/memconn v0.1.0 // indirect
+	github.com/alexbrainman/sspi v0.0.0-20210105120005-909beea2cc74 // indirect
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
+	github.com/hdevalence/ed25519consensus v0.0.0-20220222234857-c00d1f31bab3 // indirect
+	github.com/josharian/native v1.1.1-0.20230202152459-5c7d0dd6ab86 // indirect
+	github.com/jsimonetti/rtnetlink v1.1.2-0.20220408201609-d380b505068b // indirect
+	github.com/mdlayher/netlink v1.7.1 // indirect
+	github.com/mdlayher/socket v0.4.0 // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go4.org/mem v0.0.0-20210711025021-927187094b94 // indirect
+	go4.org/netipx v0.0.0-20220725152314-7e7bdc8411bf // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/cloudflare/cloudflare-go"
+	mapset "github.com/deckarep/golang-set/v2"
+	"golang.org/x/sys/unix"
+	"tailscale.com/client/tailscale"
+	"tailscale.com/ipn/ipnstate"
+)
+
+const (
+	CloudflareSyncDNSComment = "_tailscale"
+	CloudflareDomainSuffix   = ".int"
+	SyncInternal             = 30 * time.Second
+)
+
+var (
+	ctx    context.Context
+	lc     tailscale.LocalClient
+	st     *ipnstate.Status
+	api    *cloudflare.API
+	zoneID string
+	stop   context.CancelFunc
+)
+
+func init() {
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	ctx, stop = signal.NotifyContext(context.Background(), unix.SIGTERM, unix.SIGINT)
+
+	var err error
+	// init ts local client
+	st, err = lc.Status(ctx)
+	if err != nil {
+		panic(err)
+	}
+	// init cloudflare client
+	api, err = cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+	// get zone id
+	zoneID, err = api.ZoneIDByName(os.Getenv("CLOUDFLARE_DOMAIN"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getName(name string) string {
+	// normalize name
+	s := strings.Split(strings.ToLower(name), ".")
+	if len(s) > 0 {
+		return s[0]
+	}
+	return ""
+}
+
+func sync(ctx context.Context) {
+	log.Printf("sync start")
+	// name => ip string
+	tsMap := map[string]string{}
+	ts := mapset.NewSet[string]()
+	// name => record id
+	cfMap := map[string]string{}
+	cf := mapset.NewSet[string]()
+	for _, ps := range st.Peer {
+		name := getName(ps.DNSName)
+		if name != "" {
+			ts.Add(name)
+			// now only support ipv4
+			for _, ip := range ps.TailscaleIPs {
+				if ip.Is4() {
+					tsMap[name] = ip.String()
+				}
+			}
+		}
+	}
+	records, _, err := api.ListDNSRecords(ctx, cloudflare.ZoneIdentifier(zoneID), cloudflare.ListDNSRecordsParams{
+		Comment: CloudflareSyncDNSComment,
+		ResultInfo: cloudflare.ResultInfo{
+			// cloudflare limit 1000 records per page
+			PerPage: 1000,
+		},
+	})
+	if err != nil {
+		log.Printf("ListDNSRecords: %+v", err)
+		return
+	}
+	for _, r := range records {
+		name := getName(r.Name)
+		if name != "" {
+			cf.Add(getName(r.Name))
+			cfMap[name] = r.ID
+		}
+	}
+	needToSync := ts.SymmetricDifference(cf).ToSlice()
+	if len(needToSync) == 0 {
+		log.Printf("no host need to sync")
+		return
+	}
+	for _, name := range ts.SymmetricDifference(cf).ToSlice() {
+		if ts.Contains(name) {
+			ip, ok := tsMap[name]
+			if !ok {
+				continue
+			}
+			log.Printf("%s need to add to cf", name)
+			_, err := api.CreateDNSRecord(ctx, cloudflare.ZoneIdentifier(zoneID), cloudflare.CreateDNSRecordParams(cloudflare.DNSRecord{
+				Type:    "A",
+				Name:    fmt.Sprintf("%s"+CloudflareDomainSuffix, name),
+				Content: ip,
+				Comment: CloudflareSyncDNSComment,
+				TTL:     1,
+			}))
+			if err != nil {
+				log.Printf("CreateDNSRecord: %+v", err)
+				continue
+			}
+			log.Printf("%s added to cf", name)
+		}
+		if cf.Contains(name) {
+			recordID, ok := cfMap[name]
+			if !ok {
+				continue
+			}
+			log.Printf("%s need to remove from cf", name)
+			err := api.DeleteDNSRecord(ctx, cloudflare.ZoneIdentifier(zoneID), recordID)
+			if err != nil {
+				log.Printf("DeleteDNSRecord: %+v", err)
+				continue
+			}
+			log.Printf("%s removed from cf", name)
+		}
+	}
+	log.Printf("sync end")
+}
+
+func main() {
+	defer stop()
+	ticker := time.NewTicker(SyncInternal)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			sync(ctx)
+			ticker.Reset(SyncInternal)
+		case <-ctx.Done():
+			log.Println("sync stopped")
+			return
+		}
+	}
+}
